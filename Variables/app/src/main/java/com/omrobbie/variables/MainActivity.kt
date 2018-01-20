6ff990ed779a037cbe9edef4d53c9fcc1be3d65b
omrobbie <putu.roby@yahoo.com>
2018-01-20 23:42:16 +0800

    introducing to var and val

--- Variables/app/src/main/java/com/omrobbie/variables/MainActivity.kt
+++ Variables/app/src/main/java/com/omrobbie/variables/MainActivity.kt
@@ -8,5 +8,57 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var1()
+        var2()
+        var3()
+        var4()
+    }
+
+    fun var1() {
+        var num1 = 8
+        var num2 = 9
+        var name = "omrobbie"
+        var f = 2.1
+
+        var sum = num1 + num2 + f
+        println(sum)
+        println(name)
+    }
+
+    fun var2() {
+        var num1: Int
+        var name: String
+
+        num1 = 87
+        name = "omrobbie"
+
+        println(num1)
+        println(name)
+    }
+
+    fun var3() {
+        // ? for allowing null value
+        var num1: Int?
+        var name: String?
+
+        num1 = null
+        name = null
+        println(num1 + " | " + name)
+
+        num1 = 9
+        name = "omrobbie"
+        println("$num1 | $name")
+    }
+
+    fun var4() {
+        var name: String = "Kotlin"
+        val num1: Int = 10 //val = cannot update the value
+
+        name = "omrobbie"
+        //num1 = 8
+
+        println(num1)
+        println(name)
     }
 }
